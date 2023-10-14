800a3a7388d7a3f2782cc60a4da7ca1201bbd586
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2023-10-14 21:49:15 +0700

    [Feat]: removing unnecessaries code

--- Ex1-Todo-App/script.js
+++ Ex1-Todo-App/script.js
@@ -1,13 +1,3 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
