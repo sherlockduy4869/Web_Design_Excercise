635e1b0343fae9dc092d9ce0b2d1de96b8b18019
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2023-10-16 15:37:18 +0700

    [Feat]: completing ex03

---
+++ Ex3-Todo-App/script.js
@@ -0,0 +1,197 @@
+async function crudTask() {
+
+  const urlBased = 'https://eb12f861-97fb-4590-b42f-d8a6cb9178d9.mock.pstmn.io';
+
+  let listTodoJob = await fetch(
+    `${urlBased}/tasks`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  var list = document.querySelector("ul");
+  list.addEventListener(
+    "click",
+    async function (ev) {
+      var currentTask = ev.target.textContent.slice(0, -1);
+      if (ev.target.tagName === "LI") {
+        for (i = 0; i < listTodoJob.length; i++) {
+          if (
+            listTodoJob[i].taskName === currentTask &&
+            listTodoJob[i].status === "NOT_DONE"
+          ) {
+            const data = {
+              taskName: listTodoJob[i].taskName,
+              status: "DONE",
+            };
+
+            const markDoneTask = await fetch(
+              `${urlBased}/tasks`,
+              {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+              }
+            )
+              .then((res) => {
+                return res.json();
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+
+            console.log(markDoneTask);
+
+            ev.target.classList.add("checked");
+          } else if (
+            listTodoJob[i].taskName === currentTask &&
+            listTodoJob[i].status === "DONE"
+          ) {
+            const data = {
+              taskName: listTodoJob[i].taskName,
+              status: "NOT_DONE",
+            };
+
+            const markNotDoneTask = await fetch(
+              `${urlBased}/tasks/mark-not-done`,
+              {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+              }
+            )
+              .then((res) => {
+                return res.json();
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+
+            console.log(markNotDoneTask);
+            ev.target.classList.remove("checked");
+          }
+        }
+      }
+    },
+    false
+  );
+
+  function loadTaskTodo() {
+    for (i = 0; i < listTodoJob.length; i++) {
+      if (listTodoJob[i].status !== "DELETED") {
+        var li = document.createElement("li");
+        var t = document.createTextNode(listTodoJob[i].taskName);
+        li.appendChild(t);
+        document.getElementById("myUL").appendChild(li);
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        if (listTodoJob[i].status === "DONE") {
+          li.classList.add("checked");
+        }
+      }
+    }
+  }
+  loadTaskTodo();
+
+  var close = document.getElementsByClassName("close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = async function () {
+      var blockParent = this.parentElement;
+      currentTask = blockParent.textContent.slice(0, -1);
+      for (i = 0; i < listTodoJob.length; i++) {
+        if (listTodoJob[i].taskName === currentTask) {
+          const data = {
+            taskName: listTodoJob[i].taskName,
+            status: "DELETED",
+          };
+
+          const deletedTask = await fetch(
+            `${urlBased}/tasks/delete-task`,
+            {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(data),
+            }
+          )
+            .then((res) => {
+              return res.json();
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+
+          console.log(deletedTask);
+        }
+      }
+      blockParent.style.display = "none";
+    };
+  }
+
+  var addBtn = document.querySelector('.addBtn');
+
+  addBtn.addEventListener("click", async function () {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === "") {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    const data = {
+      taskName: inputValue,
+      status: "NOT_DONE",
+    };
+
+    const newTask = await fetch(
+      `${urlBased}/tasks`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log(newTask);
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function () {
+        var blockParent = this.parentElement;
+        blockParent.style.display = "none";
+      };
+    }
+  });
+}
+
+crudTask();
