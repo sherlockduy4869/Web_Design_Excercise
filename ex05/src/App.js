dd8540882cc7f14b3519fd4845bd6a831882d77f
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2023-10-30 20:24:08 +0700

    [feat]: completing ui

---
+++ ex05/src/App.js
@@ -0,0 +1,12 @@
+import Show from './components/Show/show.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Show/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ex05/src/components/Show/show.jsx
@@ -0,0 +1,11 @@
+import Accordion from '../Accordion/accordion';
+
+const show = () => {
+  return (
+    <div>
+        <Accordion/>
+    </div>
+  )
+}
+
+export default show
